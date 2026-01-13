99f84abb93fb40980ab77dd0d3d6c7f056ec234b
ragaes-ui <ragaesa75@gmail.com>
2026-01-13 16:09:18 +0700

    Enhance user schema with email and role fields
    Added email and role fields to user schema.

--- models/users.js
+++ models/users.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true } // Nanti isinya password yang udah di-hash
+    username: { type: String, required: true },
+    email: { type: String, unique: true }, // Tambahan buat User
+    password: { type: String, required: true },
+    role: { type: String, default: 'user' } // 'admin' atau 'user'
 });
 
 module.exports = mongoose.model('User', userSchema);
