d083efd4b8f3b9dd70239240e02003c1c4386ca9
ragaes-ui <ragaesa75@gmail.com>
2026-01-14 09:44:55 +0700

    Fix formatting of role field in user schema

--- models/users.js
+++ models/users.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, unique: true }, // Tambahan buat User
     password: { type: String, required: true },
-    role: { type: String, default: 'user' } // 'admin' atau 'user'
+    role: { type: String, default: 'user' }, // 'admin' atau 'user'
     fullName: { type: String, default: '' },
     phone: { type: String, default: '' }
 });
