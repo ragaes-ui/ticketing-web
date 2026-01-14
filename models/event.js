1a02d8f88dd032dd1514624d967a635fc3f224ba
ragaes-ui <ragaesa75@gmail.com>
2026-01-14 17:57:18 +0700

    Enhance event schema with additional fields
    Added new fields for description, category, and location to the event schema.

--- models/event.js
+++ models/event.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
 const eventSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  date: { type: Date, required: true },
-  price: { type: Number, required: true },
-  totalCapacity: { type: Number, required: true },
-  availableSeats: { type: Number, required: true },
+    name: { type: String, required: true },
+    date: { type: Date, required: true },
+    price: { type: Number, required: true },
+    totalCapacity: { type: Number, required: true },
+    availableSeats: { type: Number, required: true },
+    
+    // --- KOLOM BARU ---
+    description: { type: String },                  // Penjelasan event
+    category: { type: String, default: 'General' }, // Kategori (Konser, Olahraga, dll)
+    location: { type: String, default: 'TBA' }      // Lokasi (ICE BSD, GBK, dll)
 });
 
 module.exports = mongoose.model('Event', eventSchema);
