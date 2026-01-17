139f8189cf7cf8d4d0bd8881264caf7b73a1cfdb
ragaes-ui <ragaesa75@gmail.com>
2026-01-17 15:40:39 +0700

    Update server.js

--- server.js
+++ server.js
@@ -299,4 +299,4 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 module.exports = app;
-app.listen(PORT, () => console.log(`🚀 Server jalan di port ${PORT}`)); server js saya
+app.listen(PORT, () => console.log(`🚀 Server jalan di port ${PORT}`)); 
