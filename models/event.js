785afbb436796c314a5e3866d7dbc90d516f60e1
ragaes-ui <ragaesa75@gmail.com>
2026-02-26 12:17:06 +0700

    Update event.js

--- models/event.js
+++ models/event.js
@@ -54,7 +54,16 @@ const eventSchema = new mongoose.Schema({
     image: { 
         type: String, 
         default: "" 
-    }
+    },
+    // 👇 --- TAMBAHAN BARU UNTUK TIPE TIKET (TIERING) --- 👇
+    // Array ini akan menyimpan macam-macam jenis tiket di dalam 1 event
+    tickets: [{
+        tierName: { type: String, required: true },      // Contoh: "Presale 1", "VIP", atau "Sharing 1 Bulan"
+        price: { type: Number, required: true },         // Harga spesifik untuk kategori ini
+        totalSeats: { type: Number, required: true },    // Kuota khusus kategori ini
+        availableSeats: { type: Number, required: true } // Sisa tiket kategori ini
+    }]
+    // ----------------------------------------------------
     
     // ---------------------------
     
